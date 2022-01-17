93a7177dfba26563e694b10d173557e8b670b70b
Khom <mgrprinc37@gmail.com>
2022-01-17 16:47:09 +0545

    ikhom: committed some changes

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ audio = AudioCaptcha()
 
 data = audio.generate('1234')
 audio.write('1234', '1.wav')
+# data = audio.generate('abcd')
+# audio.write('abcd', '1.wav')
 
 data = image.generate('1234')
 image.write('1234', '1.png')
+# data = image.generate('abcd')
+# image.write('abcd', '1.png')
 
 
 # if __name__ == '__main__':
