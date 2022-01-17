608b6eef5b3048074dff78981a1256d360590bf7
Khom <mgrprinc37@gmail.com>
2022-01-17 16:39:58 +0545

    ikhom: generated CAPTCHA both imageCAPTCHA and audioCAPTCHA

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+'''
+@author: KhomZ
+'''
+from captcha.image import ImageCaptcha
+from captcha.audio import AudioCaptcha
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+image = ImageCaptcha()
+audio = AudioCaptcha()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+data = audio.generate('1234')
+audio.write('1234', '1.wav')
+
+data = image.generate('1234')
+image.write('1234', '1.png')
+
+
+# if __name__ == '__main__':
+#     main()
